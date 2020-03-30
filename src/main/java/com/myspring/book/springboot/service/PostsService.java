a17c41a7d564e6f6efd4abe8d836f2ba404b700d
[udon11] <[udon1@naver.com]>
2020-03-31 00:42:51 +0900

    20200331 update,insert

--- src/main/java/com/myspring/book/springboot/service/PostsService.java
+++ src/main/java/com/myspring/book/springboot/service/PostsService.java
@@ -2,13 +2,18 @@ package com.myspring.book.springboot.service;
 
 import com.myspring.book.springboot.domain.posts.Posts;
 import com.myspring.book.springboot.domain.posts.PostsRepository;
+import com.myspring.book.springboot.web.dto.PostsListResponseDto;
 import com.myspring.book.springboot.web.dto.PostsResponseDto;
 import com.myspring.book.springboot.web.dto.PostsSaveRequestDto;
 import com.myspring.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor    // final선언된애들 생성자 생성
 @Service
@@ -33,4 +38,13 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)// (readOnly = true) 트랜잭션범위유지하되 조회기능만 남겨서 조회속도가 개선 (등록,수정,삭제 기능 전혀없는 서비스 메소드에 사용하는것추천)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    //public List<Posts>
 }
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,8 +1,12 @@
-var index = {
+var main = {
     init : function () {
         var _this = this;
         $('#btn-save').on('click', function() {
             _this.save();
+        });
+
+        $('#btn-update').on('click', function(){
+            _this.update();
         });
     },
     save : function () {
@@ -24,7 +28,28 @@ var index = {
         }).fail(function(error){
             alert(JSON.stringify(error));
         });
+    },
+    update : function () {
+        var date = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $ajax({
+            type: 'PUT',    //p.154 왜 put메소드를 선택?
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function(){
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error){
+            alert(JSON.stringify(error))
+        });
     }
 };
 
-index.init();
+main.init();
