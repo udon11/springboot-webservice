c56b9650f39d9caa6e8874eecdfb05c2bcdeeafc
[udon11] <[udon1@naver.com]>
2020-03-31 23:28:08 +0900

    google login

---
+++ src/main/java/com/myspring/book/springboot/domain/user/User.java
@@ -0,0 +1,20 @@
+package com.myspring.book.springboot.domain.user;
+
+import com.myspring.book.springboot.domain.posts.BaseTimeEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
